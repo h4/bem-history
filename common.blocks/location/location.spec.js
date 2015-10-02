c10cf4efa2a15983880618ae5dd09ec67d1a8977
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-10-03 00:14:00 +0300

    Fix silent mode for block location

--- common.blocks/location/location.spec.js
+++ common.blocks/location/location.spec.js
@@ -1,4 +1,4 @@
-modules.define('spec', ['uri', 'location'], function(provide, Uri, location) {
+modules.define('spec', ['uri', 'location', 'sinon'], function(provide, Uri, location, sinon) {
 
 describe('location native API', function() {
 
@@ -20,6 +20,42 @@ describe('location native API', function() {
             (u.getPath() + u.getQuery()).should.be.eql('/desktop.specs/location/spec-js+browser-js+bemhtml/spec-js+browser-js+bemhtml.html?test=1&test=2&param2=22');
         });
 
+        it('shouldn\'t emit "change" event when silent option passed', function() {
+            var spy = sinon.spy();
+            var url1 = '/desktop.specs/location/spec-js+browser-js+bemhtml/spec-js+browser-js+bemhtml.html?test=a';
+            var url2 = '/desktop.specs/location/spec-js+browser-js+bemhtml/spec-js+browser-js+bemhtml.html?test=b';
+            location.on('change', spy);
+            location.change({
+                url : url1,
+                silent : true
+            });
+            location.change({
+                url : url2
+            });
+
+            spy.should.have.been.calledOnce;
+        });
+
+        it.skip('should emit "change" event when back button pressed', function() {
+            // Этот тест будет возможно запускать только на phantomjs2. В 1.9.х не эмитится событие popstate
+            // https://github.com/ariya/phantomjs/issues/11738
+            var spy = sinon.spy();
+            var url1 = '/desktop.specs/location/spec-js+browser-js+bemhtml/spec-js+browser-js+bemhtml.html?test=a';
+            var url2 = '/desktop.specs/location/spec-js+browser-js+bemhtml/spec-js+browser-js+bemhtml.html?test=b';
+            location.on('change', spy);
+            location.change({
+                url : url1,
+                silent : true
+            });
+            location.change({
+                url : url2,
+                silent : true
+            });
+
+            window.history.back();
+
+            spy.should.have.been.calledOnce;
+        });
 });
 
 provide();
